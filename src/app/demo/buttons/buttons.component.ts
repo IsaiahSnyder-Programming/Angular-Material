91a543bace62916763dd721efca6617fed8727b6
isaiahsnyder-programming <isaiahsnyder-programming@gmail.com>
2022-06-29 15:50:12 -0600

    Newly generated components

--- src/app/demo/buttons/buttons.component.ts
+++ src/app/demo/buttons/buttons.component.ts
@@ -2,13 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-buttons',
-  template: `
-  <button mat-button>
-    <mat-icon>face</mat-icon>
-    Click Me!
-  </button>
-  <mat-checkbox>Check me!</mat-checkbox>
-  `,
+  templateUrl: 'buttons.component.html',
   styles: [
   ]
 })
--- src/app/demo/demo.module.ts
+++ src/app/demo/demo.module.ts
@@ -2,21 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MaterialModule } from '../shared/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 
 import { DemoRoutingModule } from './demo-routing.module';
 import { ButtonsComponent } from './buttons/buttons.component';
+import { FlexboxComponent } from './flexbox/flexbox.component';
 
 
 @NgModule({
   declarations: [
-    ButtonsComponent
+    ButtonsComponent,
+    FlexboxComponent
   ],
   imports: [
     CommonModule,
-    DemoRoutingModule,
+    MaterialModule,
+    FlexLayoutModule,
     FormsModule,
-    MaterialModule
+    DemoRoutingModule,
   ]
 })
 export class DemoModule { }
