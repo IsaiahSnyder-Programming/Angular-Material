a54028483fedb472bfa630b4d1e18f7c99f75684
isaiahsnyder-programming <isaiahsnyder-programming@gmail.com>
2022-06-29 16:13:18 -0600

    Tiny bit of rerouting

--- src/app/contactmanager/contactmanager.module.ts
+++ src/app/contactmanager/contactmanager.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from "@angular/router";
+
 import { ContactmanagerAppComponent } from './contactmanager-app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
 
+import { MaterialModule } from '../shared/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
 
+
+const routes: Routes = [
+  { path: '', component: ContactmanagerAppComponent,
+    children: [
+      { path: '', component: MainContentComponent}
+    ]
+  },
+  { path: '**', redirectTo: 'contactmanager' }
+];
 
 @NgModule({
   declarations: [
@@ -15,7 +29,11 @@ import { SidenavComponent } from './components/sidenav/sidenav.component';
     SidenavComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    FlexLayoutModule,
+    FormsModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class ContactmanagerModule { }
